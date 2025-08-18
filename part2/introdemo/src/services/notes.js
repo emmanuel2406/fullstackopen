bd57094ef3065f9a44b89e9938c3a5d801538106
emmanuel2406 <emmanuelmichaelrassou@gmail.com>
2025-08-18 14:26:23 +0200

    Part 3 demo

--- part2/introdemo/src/services/notes.js
+++ part2/introdemo/src/services/notes.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/notes'
+// relative url since frontend and backend at same url
+const baseUrl = 'api/notes'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
