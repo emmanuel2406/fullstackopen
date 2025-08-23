f6f76bba48aef4dc89ac14d9ed7f7eb1eaab65dc
emmanuel2406 <emmanuelmichaelrassou@gmail.com>
2025-08-23 18:37:37 +0200

    Finish part 3 introdemo with mongodb

--- part3/introdemo/index.js
+++ part3/introdemo/index.js
@@ -1,13 +1,10 @@
 const express = require('express')
-const cors = require('cors')
 const app = express()
 require('dotenv').config()
 
-app.use(express.json())
-app.use(cors())
 app.use(express.static('dist'))
+app.use(express.json())
 
-const Note = require('./models/note')
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -17,6 +14,8 @@ const requestLogger = (request, response, next) => {
   next()
 }
 app.use(requestLogger)
+
+const Note = require('./models/note')
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World</h1>')
@@ -28,30 +27,44 @@ app.get('/api/notes', (request, response) => {
     })
 })
 
-app.get('/api/notes/:id', (request, response) => {
-    const id = request.params.id
-    const note = notes.find(note => note.id === id)
-    if (note) {
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
         response.json(note)
-    } else {
-        response.status(404).send('Note not found')
-    }
+      } else {
+        response.status(404).end()
+      }
+    })
+  .catch(error => next(error))
 })
 
 app.delete('/api/notes/:id', (request, response) => {
-  note = Note.findById(request.params.id).then(
-    note => {
+  Note.findByIdAndDelete(request.params.id).then(
+    result => {
       response.status(204).end()
     }
-  )
+  ).catch(error => next(error))
 })
 
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => Number(n.id)))
-    : 0
-  return String(maxId + 1)
-}
+app.put('/api/notes/:id', (request, response, next) => {
+  const { content, important } = request.body
+
+  Note.findById(request.params.id)
+    .then(note => {
+      if (!note) {
+        return response.status(404).end()
+      }
+
+      note.content = content
+      note.important = important
+
+      return note.save().then(updatedNote => {
+        response.json(updatedNote)
+      })
+    })
+    .catch(error => next(error))
+})
 
 app.post('/api/notes', (request, response) => {
   const body = request.body
@@ -78,6 +91,19 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler)
 
 
 const PORT = process.env.PORT
--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const morgan = require('morgan')
+require('dotenv').config()
 const app = express()
+
+const Person = require('./models/person')
 
 app.use(express.static('dist'))
 
@@ -27,31 +30,10 @@ app.use(morgan('conditional'))
 app.use(express.json())
 
 
-persons = [
-    {
-      "id": "1",
-      "name": "Arto Hellas",
-      "number": "040-123456"
-    },
-    {
-      "id": "2",
-      "name": "Ada Lovelace",
-      "number": "39-44-5323523"
-    },
-    {
-      "id": "3",
-      "name": "Dan Abramov",
-      "number": "12-43-234345"
-    },
-    {
-      "id": "4",
-      "name": "Mary Poppendieck",
-      "number": "39-23-6423122"
-    }
-]
-
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
 })
 
 app.get('/api/persons/:id', (request, response) => {
@@ -75,10 +57,12 @@ app.get('/info', (request, response) => {
         second: '2-digit',
         timeZoneName: 'long'
     })
-    response.send(`
-        <p>Phonebook has info for ${persons.length} people</p>
-        <p>${time}</p>
-    `)
+    Person.countDocuments().then(count => {
+        response.send(`
+            <p>Phonebook has info for ${count} people</p>
+            <p>${time}</p>
+        `)
+    })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -88,9 +72,6 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
 })
 
-const generateId = () => {
-    return String(Math.round(Math.random() * 10000))
-}
 app.post('/api/persons', (request, response) => {
     const body = request.body
     if (!body.name) {
@@ -104,28 +85,21 @@ app.post('/api/persons', (request, response) => {
             error: "number missing"
         })
     }
-
-    const person_found = persons.find(person => person.name === body.name)
-    if (person_found) {
-        return response.status(400).json({
-            error: "name must be unique"
-        })
-    }
-    const person = {
+    const person = new Person({
         name: body.name,
         number: body.number,
-        id: generateId()
-    }
+    })
 
-    persons = persons.concat(person)
-    response.json(person)
+    person.save().then(savedPerson => {
+        response.json(savedPerson)
+    })
 })
 
 // Catch all routes not listed above
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
   }
-  app.use(unknownEndpoint)
+app.use(unknownEndpoint)
 
 PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
