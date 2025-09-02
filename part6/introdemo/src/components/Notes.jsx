5d24f9a7843ee808d828bc4c69028a444b164532
emmanuel2406 <emmanuelmichaelrassou@gmail.com>
2025-09-02 17:28:57 -0400

    Part 6 intro to redux

---
+++ part6/introdemo/src/components/Notes.jsx
@@ -0,0 +1,35 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { toggleImportanceOf } from '../reducers/noteReducer'
+
+// presentation component
+const Note = ({ note, handleClick }) => {
+  return(
+    <li onClick={handleClick}>
+      {note.content} 
+      <strong> {note.important ? 'important' : ''}</strong>
+    </li>
+  )
+}
+
+// container component
+const Notes = () => {
+
+  const dispatch = useDispatch()
+  const notes = useSelector(state => state)
+
+  return(
+    <ul>
+      {notes.map(note =>
+        <Note
+          key={note.id}
+          note={note}
+          handleClick={() => 
+            dispatch(toggleImportanceOf(note.id))
+          }
+        />
+      )}
+    </ul>
+  )
+}
+
+export default Notes
--- part6/introdemo/src/main.jsx
+++ part6/introdemo/src/main.jsx
@@ -1,39 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import { createStore } from 'redux'
+
+import { Provider } from 'react-redux'
+
+import App from './App'
 import noteReducer from './reducers/noteReducer'
-
-
 
 const store = createStore(noteReducer)
 
-store.dispatch({
-  type: 'NEW_NOTE',
-  payload: {
-    content: 'the app state is in redux store',
-    important: true,
-    id: 1
-  }
-})
-
-store.dispatch({
-  type: 'NEW_NOTE',
-  payload: {
-    content: 'state changes are made with actions',
-    important: false,
-    id: 2
-  }
-})
-
-const App = () => {
-  return(
-    <div>
-      <ul>
-        {store.getState().map(note=>
-          <li key={note.id}>
-            {note.content} <strong>{note.important ? 'important' : ''}</strong>
-          </li>
-        )}
-        </ul>
-    </div>
-  )
-}
-export default noteReducer;
+ReactDOM.createRoot(document.getElementById('root')).render(
+  // Provider is the magic - instead of prop drilling, we can use it to auto-pass the store to all children
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
