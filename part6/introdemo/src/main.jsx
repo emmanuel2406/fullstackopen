a9656ed954b5e1f214a7bc01b5be43cc7cc0ead9
emmanuel2406 <emmanuelmichaelrassou@gmail.com>
2025-09-03 20:36:34 -0400

    Part 6 redux toolkit + devtools

--- part6/introdemo/src/main.jsx
+++ part6/introdemo/src/main.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createStore, combineReducers } from 'redux'
+import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 
 import App from './App'
 import noteReducer from './reducers/noteReducer'
 import filterReducer from './reducers/filterReducer'
 
-const reducer = combineReducers({
-  notes: noteReducer,
-  filter: filterReducer
+const store = configureStore({
+  reducer: {
+    notes: noteReducer,
+    filter: filterReducer
+  }
 })
-
-const store = createStore(reducer)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // Provider is the magic - instead of prop drilling, we can use it to auto-pass the store to all children
