c23a1bdccf7ea5d4596c59cda2604d5001f2cc52
emmanuel2406 <emmanuelmichaelrassou@gmail.com>
2025-12-29 18:27:50 +0400

    Part 8 React and GraphQL

---
+++ part8/introdemo/client/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { useQuery } from "@apollo/client/react";
+import Persons from "./components/Persons";
+import PersonForm from "./components/PersonForm";
+import Notify from "./components/Notify";
+import PhoneForm from "./components/PhoneForm";
+
+import { ALL_PERSONS } from "./queries";
+
+const App = () => {
+  const result = useQuery(ALL_PERSONS);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  if (result.loading) {
+    return <div>loading...</div>;
+  }
+
+  const notify = (message) => {
+    setErrorMessage(message);
+    setTimeout(() => {
+      setErrorMessage(null);
+    }, 5000);
+  };
+
+  return (
+    <div>
+      <Notify errorMessage={errorMessage} />
+      <Persons persons={result.data.allPersons} />
+      <PersonForm setError={notify} />
+      <PhoneForm setError={notify} />
+    </div>
+  );
+};
+
+export default App;
