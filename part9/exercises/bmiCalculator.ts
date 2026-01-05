c1728703a07c6586c327c6a0341a8c57843df2f6
emmanuel2406 <emmanuelmichaelrassou@gmail.com>
2026-01-05 12:31:14 +0200

    ...Ex 9.3

---
+++ part9/exercises/bmiCalculator.ts
@@ -0,0 +1,62 @@
+interface BmiValues {
+  height: number;
+  weight: number;
+}
+
+const parseBmiArguments = (args: string[]): BmiValues => {
+  if (args.length < 4) throw new Error("Not enough arguments");
+  if (args.length > 4) throw new Error("Too many arguments");
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return { height: Number(args[2]), weight: Number(args[3]) };
+  } else {
+    throw new Error("Provided values were not numbers");
+  }
+};
+
+const formulaBmi = (height_cm: number, weight_kg: number): number => {
+  const height_m: number = height_cm / 100;
+  if (height_m <= 0 || weight_kg <= 0) {
+    throw new Error("Height and weight must be positive");
+  }
+  const bmi: number = weight_kg / (height_m * height_m);
+  return bmi;
+};
+
+const getBmiCategory = (bmi: number): string => {
+  if (bmi < 0) {
+    throw new Error("BMI cannot be negative");
+  } else if (bmi < 16.0) {
+    return "Underweight (Severe thinness)";
+  } else if (bmi < 17.0) {
+    return "Underweight (Moderate thinness)";
+  } else if (bmi < 18.5) {
+    return "Underweight (Mild thinness)";
+  } else if (bmi < 25.0) {
+    return "Normal range";
+  } else if (bmi < 30.0) {
+    return "Overweight (Pre-obese)";
+  } else if (bmi < 35.0) {
+    return "Obese (Class I)";
+  } else if (bmi < 40.0) {
+    return "Obese (Class II)";
+  } else {
+    return "Obese (Class III)";
+  }
+};
+
+const calculateBmi = (height_cm: number, weight_kg: number): string => {
+  const bmi: number = formulaBmi(height_cm, weight_kg);
+  const bmiCategory: string = getBmiCategory(bmi);
+  return bmiCategory;
+};
+
+try {
+  const { height, weight } = parseBmiArguments(process.argv);
+  console.log(calculateBmi(height, weight));
+} catch (error: unknown) {
+  let errorMessage = "Something went wrong: ";
+  if (error instanceof Error) {
+    errorMessage += error.message;
+  }
+  console.log(errorMessage);
+}
---
+++ part9/exercises/exerciseCalculator.ts
@@ -0,0 +1,73 @@
+interface Result {
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: string;
+  target: number;
+  average: number;
+}
+
+interface ExerciseValues {
+  dailyExercises: number[];
+  target: number;
+}
+
+const parseExerciseArguments = (args: string[]): ExerciseValues => {
+  if (args.length < 4) throw new Error("Not enough arguments");
+  for (let i = 2; i < args.length - 1; i++) {
+    if (isNaN(Number(args[i]))) {
+      throw new Error("Provided values were not numbers");
+    }
+  }
+  return {
+    dailyExercises: args.slice(3).map(Number),
+    target: Number(args[2]),
+  };
+};
+
+const getRatingDescription = (rating: number): string => {
+  switch (rating) {
+    case 3:
+      return "Great job! You've reached your target.";
+    case 2:
+      return "Not too bad but could be better.";
+    case 1:
+      return "You didn't exercise enough this period.";
+    default:
+      throw new Error("Invalid rating");
+  }
+};
+
+const calculateExercises = (
+  dailyExercises: number[],
+  target: number
+): Result => {
+  const periodLength: number = dailyExercises.length;
+  const trainingDays: number = dailyExercises.filter((day) => day > 0).length;
+  const average: number =
+    dailyExercises.reduce((sum, day) => sum + day, 0) / dailyExercises.length;
+  const success: boolean = average >= target;
+  const rating: number = success ? 3 : average >= target / 2 ? 2 : 1;
+  const ratingDescription: string = getRatingDescription(rating);
+  return {
+    periodLength,
+    trainingDays,
+    success,
+    rating,
+    ratingDescription,
+    target,
+    average,
+  };
+};
+
+try {
+  const { dailyExercises, target } = parseExerciseArguments(process.argv);
+  console.log(calculateExercises(dailyExercises, target));
+} catch (error: unknown) {
+  let errorMessage = "Something went wrong: ";
+  if (error instanceof Error) {
+    errorMessage += error.message;
+  }
+  console.log(errorMessage);
+}
