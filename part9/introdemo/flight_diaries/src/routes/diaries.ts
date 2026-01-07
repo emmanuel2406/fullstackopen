93bfc7e1a7442f4dbc89b71a21f5fe91cab92512
emmanuel2406 <emmanuelmichaelrassou@gmail.com>
2026-01-07 11:23:00 +0200

    Part 9 Typing an express app

--- part9/introdemo/flight_diaries/src/routes/diaries.ts
+++ part9/introdemo/flight_diaries/src/routes/diaries.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { Response } from "express";
 import diaryService from "../services/diaryService";
 import { NonSensitiveDiaryEntry } from "../types";
+import toNewDiaryEntry from "../utils";
 
 const router: express.Router = express.Router();
 
@@ -9,8 +10,21 @@ router.get("/", (_req, res: Response<NonSensitiveDiaryEntry[]>) => {
   res.send(diaryService.getNonSensitiveEntries());
 });
 
-router.post("/", (_req, res) => {
-  res.send("Saving a diary!");
+router.get("/:id", (req, res) => {
+  const diary = diaryService.findById(parseInt(req.params.id));
+
+  if (diary) {
+    res.send(diary);
+  } else {
+    res.status(404).send({ error: "Diary not found" });
+  }
+});
+
+router.post("/", (req, res) => {
+  const newDiaryEntry = toNewDiaryEntry(req.body);
+
+  const addedEntry = diaryService.addDiary(newDiaryEntry);
+  res.json(addedEntry);
 });
 
 export default router;
