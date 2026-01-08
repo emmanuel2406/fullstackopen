cd717b8b45330e4b5ad6371b889b828be25b800a
emmanuel2406 <emmanuelmichaelrassou@gmail.com>
2026-01-08 15:03:55 +0200

    Part 9 React with Types II

---
+++ part9/introdemo/note-app/src/services/noteService.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+import { Note, NewNote } from "../types";
+
+const baseUrl = "http://localhost:3001/notes";
+
+export const getAllNotes = async () => {
+  const response = await axios.get<Note[]>(baseUrl);
+  return response.data;
+};
+
+export const createNote = (object: NewNote) => {
+  return axios.post<Note>(baseUrl, object).then((response) => response.data);
+};
