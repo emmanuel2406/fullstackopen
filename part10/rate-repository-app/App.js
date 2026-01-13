79dd5473b2953ddab93ea1fb293b20492e1640d9
emmanuel2406 <emmanuelmichaelrassou@gmail.com>
2026-01-13 18:02:17 +0200

    Part 10 Communicating with server IV

--- part10/rate-repository-app/App.js
+++ part10/rate-repository-app/App.js
@@ -1,21 +1,23 @@
 import { NativeRouter } from "react-router-native";
 import { ApolloProvider } from "@apollo/client/react";
-import Constants from "expo-constants";
 
 import Main from "./src/components/Main";
+import AuthStorage from "./src/utils/authStorage";
 import createApolloClient from "./src/utils/apolloClient";
+import AuthStorageContext from "./src/contexts/AuthStorageContext";
 
-const apolloClient = createApolloClient();
+const authStorage = new AuthStorage();
+const apolloClient = createApolloClient(authStorage);
 
 const App = () => {
-  console.log(Constants.expoConfig);
-
   return (
-    <NativeRouter>
-      <ApolloProvider client={apolloClient}>
-        <Main />
-      </ApolloProvider>
-    </NativeRouter>
+    <AuthStorageContext.Provider value={authStorage}>
+      <NativeRouter>
+        <ApolloProvider client={apolloClient}>
+          <Main />
+        </ApolloProvider>
+      </NativeRouter>
+    </AuthStorageContext.Provider>
   );
 };
 
--- part10/rate-repository-app/src/hooks/useSignIn.js
+++ part10/rate-repository-app/src/hooks/useSignIn.js
@@ -1,14 +1,18 @@
 import { useMutation } from "@apollo/client/react";
 
+import useAuthStorage from "./useAuthStorage";
+
 import { LOGIN } from "../graphql/mutations";
 
 const useSignIn = () => {
+  const authStorage = useAuthStorage();
   const [mutate, result] = useMutation(LOGIN);
 
   const signIn = async ({ username, password }) => {
     const result = await mutate({
       variables: { credentials: { username, password } },
     });
+    await authStorage.setAccessToken(result.data.authenticate.accessToken);
     return result;
   };
 
