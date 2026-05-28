630b75b077de6cb12d239c7f7cd6d73a6d0308c3
emmanuel2406 <emmanuelmichaelrassou@gmail.com>
2026-05-27 23:40:17 -0400

    Part 12 Building and configuring environments

---
+++ part12/fs-containers/todo-app/todo-backend/routes/statistics.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const redis = require("../redis");
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  const addedTodos = await redis.get("added_todos");
+  res.send({ added_todos: addedTodos });
+});
+
+module.exports = router;
